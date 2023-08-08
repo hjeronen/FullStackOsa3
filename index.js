d997750026b3467c59017cc9c52e78435e120d60
Heli Eronen <herojonen@gmail.com>
2023-08-08 16:09:41 +0300

    Tehtävä 3.15

--- index.js
+++ index.js
@@ -58,11 +58,17 @@ app.get('/api/persons/:id', (req, res) => {
   })
 })
 
-// app.delete('/api/persons/:id', (req, res) => {
-//   const id = Number(req.params.id)
-//   persons = persons.filter(person => person.id !== id)
-//   res.status(204).end()
-// })
+app.delete('/api/persons/:id', (req, res) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => {
+      res.status(400).json({
+        error: error
+      })
+    })
+})
 
 // const generateId = (max) => {
 //   let id = Math.floor(Math.random() * max)
